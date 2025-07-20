b7a5777e34b60e19cfcba1dcfd217d1bbf9d081a
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-07-20 21:36:03 +0800

    Add project files.

---
+++ BattleshipLiteLibrary/Models/PlayerInfoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary.Models
+{
+    public class PlayerInfoModel
+    {
+        public string Name { get; set; }
+        public List<GridSpotModel> location = new List<GridSpotModel>();
+        public List<GridSpotModel> shotgrid = new List<GridSpotModel>();
+
+    }
+}
